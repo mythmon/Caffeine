7422303a1dd1233b36ec1a7ea26d58383fb40592
Mike Cooper <mythmon@gmail.com>
2010-12-31 15:00:55 -0800

    Now its a python package!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Caffeine',
+    version='0.1',
+    author='Mike Cooper',
+    author_email='mythmon@gmail.com',
+    url='http://www.github.com/mythmon/caffeine',
+    packages=['caffeine',],
+    license='MIT/X11',
+    long_description=open('README').read(),
+)
